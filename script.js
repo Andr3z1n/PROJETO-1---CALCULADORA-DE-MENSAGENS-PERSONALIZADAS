917711bc5842e8fc0fbf7419baa760e7a5cf9318
Andr3z1n <andre.soares12@aluno.ce.gov.br>
2025-08-08 14:44:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const nome = "Andre"
+let idade = "16"
+let cidade = "Fortaleza"
+let profissão = "Engenharia mecânica"
+let anoAtual = 2025
+const Anonascimento = anoAtual - idade
+
+let salarioatual = 1500
+let bonus = 800
+let soma = bonus + salarioatual
+let totalano = soma*12
+let totalanodobro = totalano*2
+
+alert(`========= RELÁTORIO DO USUÁRIO =========
+    Nome = ${nome}
+    idade = ${idade}
+    Anonascimento = ${Anonascimento-16}
+    Profissão = ${Profissão}
+    salario base: = U$${bonus}
+    Bonus = U$${Bonus}
+    Totalmensal = U$${Totalmensal}
+    Total ano = U$${totalano}
+    Total anual = U$${totalanual}
+    =========================================`)
